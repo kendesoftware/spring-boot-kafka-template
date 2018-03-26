4b5fb863256a6912de2e3fcf3d1ed8908fbda751
Patryk Duński <patryk.dunski@kende.pl>
2018-03-26 21:30:00 +0200

    Init

---
+++ src/main/java/pl/kende/springbootkafkatemplate/SpringBootKafkaTemplateApplication.java
@@ -0,0 +1,12 @@
+package pl.kende.springbootkafkatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaTemplateApplication.class, args);
+	}
+}
